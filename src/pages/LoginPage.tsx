30ba66a607ca4eb1dc824e8dca30daa0bf4c72ad
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-05 01:23:55 +0900

    feat: ログイン画面を作成

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,28 @@
+import { PrimaryBtn } from "../components/atoms/PrimaryBtn";
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="w-[500px] bg-white rounded-lg shadow-lg py-10">
+      <form className="flex flex-col justify-center items-center gap-10">
+        <h1 className="text-3xl text-lime-800 font-bold text-center">
+          ログイン
+        </h1>
+        <div className="w-[80%]">
+          <input
+            type="text"
+            className="w-full border-4 border-solid border-lime-800 rounded-md p-2"
+            placeholder="email"
+          />
+        </div>
+        <div className="w-[80%]">
+          <input
+            type="password"
+            className="w-full border-4 border-solid border-lime-800 rounded-md p-2"
+            placeholder="password"
+          />
+        </div>
+        <PrimaryBtn>ログイン</PrimaryBtn>
+      </form>
+    </div>
+  );
 };
