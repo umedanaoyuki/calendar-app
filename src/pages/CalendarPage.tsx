f5cc00bfdf6e0b227305c9039404bb9f03c84da8
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-05 22:18:38 +0900

    feat: date-fnsを導入し、1ヶ月カレンダーを作成

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,12 +1,63 @@
-import { useRecoilValue } from "recoil";
-import { loginUserState } from "../store/loginUserState";
+import {
+  eachDayOfInterval,
+  eachWeekOfInterval,
+  endOfMonth,
+  endOfWeek,
+  getDate,
+  getMonth,
+  startOfMonth,
+} from "date-fns";
+import { DAYS_LIST } from "../constants/calendar";
+import { useEffect, useState } from "react";
 
 export const CalendarPage = () => {
-  const loginUser = useRecoilValue(loginUserState);
+  const today = new Date();
+  const [dateList, setDatelist] = useState<Date[][]>([]);
+
+  useEffect(() => {
+    const monthOfSundayList = eachWeekOfInterval({
+      start: startOfMonth(today),
+      end: endOfMonth(today),
+    });
+    const newDateList: Date[][] = monthOfSundayList.map((date) => {
+      return eachDayOfInterval({
+        start: date,
+        end: endOfWeek(date),
+      });
+    });
+    setDatelist(newDateList);
+  }, []);
+
   return (
-    <div>
-      <p>{loginUser.id}</p>
-      <p>{loginUser.name}</p>
-    </div>
+    <>
+      <h1 className=" font-bold text-3xl mb-5">{`${getMonth(today) + 1}月`}</h1>
+      <table className="w-[80%] border-collapse border-2 border-solid border-lime-800 table-fixed">
+        <thead>
+          <tr className="bg-lime-800 text-white rounded-tl-lg rounded-tr-lg py-10">
+            {DAYS_LIST.map((day) => (
+              <th key={day} className="text-center text-xl py-3">
+                {day}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {dateList.map((oneWeek) => (
+            <tr key={`week-${getDate(oneWeek[0])}`} className="mx-10">
+              {oneWeek.map((item) => (
+                <td
+                  key={`day-${getDate(item)}`}
+                  className="bg-white h-[10vh] border-2 border-solid border-lime-800"
+                >
+                  <span className="inline-block w-[20px] leading-[20px] text-center">
+                    {getDate(item)}
+                  </span>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
