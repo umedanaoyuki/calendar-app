a7b86f8105d5073a800b83d6135020e652f5f094
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-08 17:10:56 +0900

    feat: カレンダーフォームにてエラーハンドリングを追加

--- src/components/organisms/CreateScheduleModal.tsx
+++ src/components/organisms/CreateScheduleModal.tsx
@@ -32,6 +32,7 @@ export const CreateScheduleModal = ({
     date: format(new Date(), "yyyy-MM-dd"),
     description: "",
   });
+  const [errorMessage, setErrorMessage] = useState("");
 
   const changeNewSchedule = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -43,6 +44,12 @@ export const CreateScheduleModal = ({
   const handleCreateSchedule = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const { title, date, description } = newSchedule;
+    if (title === "") {
+      setErrorMessage("タイトルを入力してください");
+      return;
+    } else {
+      setErrorMessage("");
+    }
     const schedule: Schedule = {
       id: 100001,
       title,
@@ -64,6 +71,11 @@ export const CreateScheduleModal = ({
         <h3 className="text-center text-3xl text-lime-800 font-bold pb-5">
           予定作成
         </h3>
+        {errorMessage !== "" && (
+          <div className="p-5 mb-5 bg-red-500 text-white text-center rounded-lg">
+            {errorMessage}
+          </div>
+        )}
         <form className="flex flex-col gap-8" onSubmit={handleCreateSchedule}>
           <div className="w-[100%] flex items-center">
             <label htmlFor="title-form" className="w-[30%] text-lime-800">
